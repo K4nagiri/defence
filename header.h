ecd6030a0878d03b5ab40916e60261a671f3ba55
K4nagiri <yexelvale69@gmail.com>
2025-05-01 22:59:23 +0800

    Add files via upload

---
+++ header.h
@@ -0,0 +1,24 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+using namespace std;
+
+class AccountManager{
+public:
+
+    bool VerifyMasterPassword();
+    void ChangeMasterPassword();
+    void AddAccount();
+    void ViewAccount();
+    void AccountManage();
+
+};
+
+
+
+
+
+
+
+
+#endif // HEADER_H
---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <windows.h>
+#include "include/header.h"
+
+
+using namespace std;
+
+int main() {
+
+    AccountManager AccountManager;
+
+
+    AccountManager.AccountManage();
+
+
+    return 0;
+}
